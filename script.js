dcba81af82d0c5e829ba1c1f0c20992121e50eb1
JOGI VENKATA SAI KIRAN <kiranjogisk123@gmail.com>
2025-11-16 13:06:13 +0530

    Added to the repo

---
+++ script.js
@@ -0,0 +1,56 @@
+const ws = new WebSocket("deployserver-production-e1ac.up.railway.app");
+
+const messagesDiv = document.getElementById("messages");
+const input = document.getElementById("input");
+const usersList = document.getElementById("users");
+
+function printLine(text) {
+    const line = document.createElement("div");
+    line.textContent = text;
+    line.style.opacity = 0;
+
+    messagesDiv.appendChild(line);
+
+    // typing animation
+    let i = 0;
+    const interval = setInterval(() => {
+        line.style.opacity = 1;
+        line.textContent = text.substring(0, i);
+        i++;
+
+        if (i > text.length) clearInterval(interval);
+    }, 10);
+
+    messagesDiv.scrollTop = messagesDiv.scrollHeight;
+}
+
+ws.onopen = () => {
+    printLine("[CONNECTED TO STAR-C2]");
+};
+
+ws.onmessage = (msg) => {
+    const data = JSON.parse(msg.data);
+
+    if (data.type === "chat") {
+        printLine(`${data.user}: ${data.text}`);
+    }
+
+    if (data.type === "users") {
+        usersList.innerHTML = "";
+        data.list.forEach(u => {
+            let li = document.createElement("li");
+            li.textContent = u;
+            usersList.appendChild(li);
+        });
+    }
+};
+
+input.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+        const message = input.value.trim();
+        if (message !== "") {
+            ws.send(JSON.stringify({ type: "chat", text: message }));
+        }
+        input.value = "";
+    }
+});
