b17d5cdd1d8d99160d91a5bba4f43e6deb761671
JOGI VENKATA SAI KIRAN <kiranjogisk123@gmail.com>
2025-11-16 13:22:25 +0530

    updated the repo

--- matrix.js
+++ matrix.js
@@ -19,7 +19,7 @@ function draw() {
     ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    ctx.fillStyle = "#00ff9f";
+    ctx.fillStyle = "rgba(0, 0, 0, 0.15)";
     ctx.font = fontSize + "px monospace";
 
     for (let i = 0; i < drops.length; i++) {
